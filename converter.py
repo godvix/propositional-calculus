efc3fae8165714ea7a5222d2a46c47afc27c9ae8
GODVIX <godvix@outlook.com>
2021-10-30 06:47:15 +0800

    Fix postfix_to_prefix

--- converter.py
+++ converter.py
@@ -22,11 +22,12 @@ class Converter(object):
         return self.postfix_to_prefix(postfix_tokens)
 
     def postfix_to_prefix(self, postfix_tokens: list) -> list:
-        stack = []
+        stack = list()
+        print(postfix_tokens)
         for token in postfix_tokens:
             if token in self.operators:
                 num_operands = self.operators[token].num_operands
-                new_expression = [token] + stack[num_operands:]
+                new_expression = [token] + sum(stack[-num_operands:], start=list())
                 del stack[-num_operands:]
             else:  # token is an operand
                 new_expression = [token]
@@ -45,5 +46,5 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
     else:
-        print(f"prefix expression: {converter.list_to_str(prefix_tokens)}")
-        print(f"postfix expression: {converter.list_to_str(postfix_tokens)}")
+        print("prefix expression:", converter.list_to_str(prefix_tokens))
+        print("postfix expression:", converter.list_to_str(postfix_tokens))
